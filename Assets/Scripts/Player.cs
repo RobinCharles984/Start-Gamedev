df76729a90b25477d4d7d7eb015dd33269ea1514
RobinCharles984 <talesprudente984@gmail.com>
2025-11-12 19:54:10 -0300

    Feature: Player system and animations 2 - Run

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,24 +4,75 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //Variables
-    public float speed;
+    #region Variables
+    //Attributs
+    [SerializeField] private float speed;
+    [SerializeField] private float runSpeed;
+    private float initialSpeed;
+    private Vector2 _direction;
+    private bool _isRunning;
 
+    //Components
     private Rigidbody2D rig;
-    private Vector2 direction;
 
+    #endregion
+
+    #region Capsules
+    public Vector2 direction
+    {
+        get { return _direction; }
+        set { _direction = value; }
+    }
+
+    public bool isRunning
+    {
+        get { return _isRunning; }
+        set { _isRunning = value; }
+    }
+    #endregion
 
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        initialSpeed = speed;
+        _isRunning = false;
     }
     private void Update()
     {
-        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        OnInput();
+
+        OnRun();
     }
 
     private void FixedUpdate()
     {
-        rig.MovePosition(rig.position + direction * speed * Time.fixedDeltaTime);
+        OnMove();
     }
+
+    #region Movement Methods
+    public void OnInput()
+    {
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    public void OnMove()
+    {
+        rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime);
+    }
+
+    public void OnRun()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+            isRunning = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = initialSpeed;
+            isRunning = false;
+        }
+    }
+
+    #endregion
 }
--- Assets/Scripts/PlayerAnim.cs
+++ Assets/Scripts/PlayerAnim.cs
@@ -4,18 +4,49 @@ using UnityEngine;
 
 public class PlayerAnim : MonoBehaviour
 {
-    //Variables
+    #region Variables
     private Player player;
+    private Animator anim;
 
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
-        player = GetComponent<Player>();    
+        player = GetComponent<Player>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        OnMove();
+        OnRun();
     }
+
+    #region Movement Methods
+    public void OnMove()
+    {
+        if (player.direction.sqrMagnitude > 0)
+        {
+            anim.SetInteger("transition", 1);
+        }
+        else
+        {
+            anim.SetInteger("transition", 0);
+        }
+
+        if (player.direction.x > 0)
+            transform.eulerAngles = new Vector2(0, 0);
+        if (player.direction.x < 0)
+            transform.eulerAngles = new Vector2(0, 180);
+    }
+
+    public void OnRun()
+    {
+        if (player.isRunning)
+        {
+            anim.SetInteger("transition", 2);
+        }
+    }
+    #endregion
 }
