74a811f6096f2b13e024de83e668b486dfc2ec7d
RobinCharles984 <talesprudente984@gmail.com>
2025-11-11 21:25:33 -0300

    Feature: Player animations 1 - idle and walking

---
+++ Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    //Variables
+    private Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Player>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
